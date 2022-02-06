dc81d12529b21df9a74ffc023ebc1ca0cdc67c26
Yusuke Wada <yusuke@kamawada.com>
2022-02-06 17:42:13 +0900

    miniflareで開発環境作った

---
+++ src/ies.js
@@ -0,0 +1,32 @@
+export const ies = [
+  {
+    name: 'yoshimura',
+    image: 'yoshimura.jpg',
+    title: '吉村家',
+  },
+  {
+    name: 'sugita',
+    image: 'sugita.jpg',
+    title: '杉田家',
+  },
+  {
+    name: 'takasago',
+    image: 'takasago.jpg',
+    title: 'たかさご家',
+  },
+  {
+    name: 'joujou',
+    image: 'joujou.jpg',
+    title: '上々家',
+  },
+  {
+    name: 'rasuta',
+    image: 'rasuta.jpg',
+    title: 'らすた',
+  },
+  {
+    name: 'dontokoi',
+    image: 'dontokoi.jpg',
+    title: 'どんとこい家',
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
 import { Hono } from 'hono'
 import { mustache } from 'hono/mustache'
 import { serveStatic } from 'hono/serve-static'
+import { ies } from './ies'
 
 const app = new Hono()
 
 app.use('*', mustache({ root: 'view' }))
 app.use('/static/*', serveStatic({ root: 'public' }))
 
-const db = [
-  {
-    name: 'yoshimura',
-    image: 'yoshimura.jpg',
-    title: '吉村家',
-  },
-  {
-    name: 'sugita',
-    image: 'sugita.jpg',
-    title: '杉田家',
-  },
-]
-
 const partials = { header: 'header', footer: 'footer' }
 
 app.get('/', (c) => {
-  return c.render('index', { ies: db }, partials)
+  return c.render('index', { ies: ies }, partials)
 })
 
 app.get('/ie/:name', (c) => {
   const name = c.req.param('name')
-  const ie = db.find((i) => i.name === name)
+  const ie = ies.find((i) => i.name === name)
   if (!ie) {
     return c.text('家系 is Not Found', 404)
   }
