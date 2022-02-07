fb60ccc17da54aed9be565a72b840d6ec669dd36
Yusuke Wada <yusuke@kamawada.com>
2022-02-08 05:35:42 +0900

    キャッシュした

--- src/model.ts
+++ src/model.ts
@@ -1,6 +1,9 @@
 declare const X_MICROCMS_API_KEY: string
 const END_POINT = 'https://iekei.microcms.io/api/v1/ies'
 
+declare let IEKEI: KVNamespace
+const KV_PREFIX: string = 'v1:api_cache:'
+
 export type Image = {
   url: string
   height: number
@@ -38,12 +41,20 @@ export const getIeByName = async (name: string): Promise<Data> => {
 }
 
 const getData = async (url: URL): Promise<Data> => {
-  const request = new Request(url.toString(), {
-    headers: {
-      'X-MICROCMS-API-KEY': X_MICROCMS_API_KEY,
-    },
-  })
-  const response = await fetch(request)
-  const data: Data = await response.json()
+  const key = KV_PREFIX + url.toString()
+  let json = await IEKEI.get(key)
+
+  if (!json) {
+    const request = new Request(url.toString(), {
+      headers: {
+        'X-MICROCMS-API-KEY': X_MICROCMS_API_KEY,
+      },
+    })
+    const response = await fetch(request)
+    json = await response.text()
+    await IEKEI.put(key, json)
+  }
+
+  const data: Data = JSON.parse(json)
   return data
 }
